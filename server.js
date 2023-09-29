1a6ed2876ed6e8ae832f1a8746e4c3da8b74f87d
Robert <robert.aleks.zuk@gmail.com>
2023-09-29 12:48:06 +0200

    ref code

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import path from "path";
 import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
-import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
@@ -23,7 +22,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(bodyParser.json());
 
 app.use(cors());
 // app.use(
