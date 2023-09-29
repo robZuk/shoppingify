c9b5d69733b002e02a763ee2c38bd1d168953da1
Robert <robert.aleks.zuk@gmail.com>
2023-09-29 12:06:33 +0200

    ref code

--- server.js
+++ server.js
@@ -23,12 +23,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: "http://127.0.0.1:5173",
-    credentials: true,
-  })
-);
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: "http://127.0.0.1:5173",
+//     credentials: true,
+//   })
+// );
 const __dirname = path.resolve();
 
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
