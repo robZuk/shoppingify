6caed1a589456c327ce1be89c83b8463b300d13f
Robert <robert.aleks.zuk@gmail.com>
2023-09-29 13:51:12 +0200

    ref code

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(cors());
 app.use(
   cors({
     origin: "https://shoppingify-frontend.onrender.com",
