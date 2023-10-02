6281136c74cf9dc683038f36c679059de83fcf91
Robert Aleksander <robert.aleks.zuk@gmail.com>
2023-10-02 07:44:40 +0200

    ref code7

--- server.js
+++ server.js
@@ -37,9 +37,9 @@ app.use(
 // );
 // app.use(
 //   cors({
-//     origin: "http://127.0.0.1:5173",
+//     origin: "http://localhost:5173",
 //     credentials: true,
-//   })
+// //   })
 // );
 const __dirname = path.resolve();
 
